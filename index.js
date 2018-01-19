57bfac7267ba005d7b2e5eea4996ac3e7a3a835f
Dimitar Kerezov <dimitar.kerezov@telerik.com>
2018-01-19 23:23:33 +0200

    http request with correct headers

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-const http = require("http");
+const request = require("request-promise-native");
+const _ = require("lodash");
+const fs = require("fs");
+
+const textToParse = fs.readFileSync("./text.txt").toString();
+// Work some stemming magic here
+const wordsToTag = ["син"/*, "четиво", "тест", "програма"*/];
+wordsToTag.forEach(word => {
+    const options = {
+        method: "POST",
+        uri: "http://dcl.bas.bg/bulnet/api/search",
+        headers: {
+            'X-Requested-With': 'XMLHttpRequest'
+        },
+        body: {
+            query: word,
+            exact: true,
+            type: "Synset",
+            "page": 1
+        },
+        // proxy: "http://localhost:8888", // Fiddler debugging
+        json: true
+    };
+
+    request
+        .post(options)
+        .then(response => {
+            const candiatePos = _(response.results)
+                .map(r => r.pos)
+                .reduce((out, i) => {
+                    out[i] = out[i] || 0;
+                    ++out[i];
+                    return out;
+                }, {});
+            console.log(candiatePos);
+        })
+        .catch(err => {
+            console.error("An error occurred", err);
+        });
+});
