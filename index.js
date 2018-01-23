fec26054a42c827950ddf9aef77e5e4958e416df
Dimitar Kerezov <dimitar.kerezov@telerik.com>
2018-01-23 23:42:50 +0200

    Create first ontology

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-// import { readFileSync } from "fs";
-
+const { readFileSync, writeFileSync } = require("fs");
 const request = require("request-promise-native");
 const _ = require("lodash");
 const fs = require("fs");
+const Thing = "THING";
+const TemplateFileName = "template.owl";
 const cheerio = require("cheerio");
 
 // const textToParse = fs.readFileSync("./text.txt").toString();
@@ -73,9 +74,9 @@ function getNymsObject(word) {
         })
         .then(responses => {
             if (responses) {
-                const hyponyms = getNyms(responses);
-                // const hypernyms = getNyms(responses, r => r.hypernym);
-                return { hyponyms/*, hypernyms*/ };
+                // const hyponyms = getNyms(responses);
+                const hypernyms = getNyms(responses, r => r.hypernym);
+                return { /*hyponyms, */hypernyms };
             }
         })
         .catch(err => {
@@ -106,7 +107,7 @@ return request(options)
         const formHashKey = match[1];
         const formHashValue = match[2];
         // TODO: get this from file
-        const textValue = "%D0%B1%D0%B0%D1%89%D0%B0%D1%82%D0%B0+%D0%B8+%D0%BC%D1%8A%D0%B6";
+        const textValue = "%D0%9E%D1%80%D0%B8%D0%B3%D0%B8%D0%BD%D0%B0%D0%BB%D1%8A%D1%82%20%D0%BE%D1%82%201975%20%D0%B3%D0%BE%D0%B4%D0%B8%D0%BD%D0%B0%3A%20BMW%20%D0%A1%D0%B5%D1%80%D0%B8%D1%8F%203%20%D0%B5%20%D0%BE%D0%BB%D0%B8%D1%86%D0%B5%D1%82%D0%B2%D0%BE%D1%80%D0%B5%D0%BD%D0%B8%D0%B5%20%D0%BD%D0%B0%20%D1%81%D0%BF%D0%BE%D1%80%D1%82%D0%BD%D0%B0%20%D0%BB%D0%B8%D0%BC%D1%83%D0%B7%D0%B8%D0%BD%D0%B0.%20%D0%9D%D0%B5%D1%83%D1%81%D1%82%D0%BE%D0%B8%D0%BC%D0%B0%D1%82%D0%B0%20%D0%BA%D0%BE%D0%BC%D0%B1%D0%B8%D0%BD%D0%B0%D1%86%D0%B8%D1%8F%20%D0%BC%D0%B5%D0%B6%D0%B4%D1%83%20%D0%B4%D0%B8%D0%BD%D0%B0%D0%BC%D0%B8%D1%87%D0%B5%D0%BD%20%D0%B4%D0%B8%D0%B7%D0%B0%D0%B9%D0%BD%2C%20%D0%BD%D0%B5%D0%BF%D0%BE%D0%B4%D1%80%D0%B0%D0%B6%D0%B0%D0%B5%D0%BC%D0%B0%20%D0%BF%D1%8A%D1%80%D0%B3%D0%B0%D0%B2%D0%B8%D0%BD%D0%B0%20%D0%B8%20%D0%B2%D0%B8%D1%81%D0%BE%D0%BA%D0%B0%20%D0%BF%D1%80%D0%B8%D0%B3%D0%BE%D0%B4%D0%BD%D0%BE%D1%81%D1%82%20%D0%B7%D0%B0%20%D0%B2%D1%81%D0%B5%D0%BA%D0%B8%D0%B4%D0%BD%D0%B5%D0%B2%D0%B8%D0%B5%D1%82%D0%BE%20%D0%B2%D0%BF%D0%B5%D1%87%D0%B0%D1%82%D0%BB%D1%8F%D0%B2%D0%B0%20%D0%B8%20%D0%B2%20%D1%88%D0%B5%D1%81%D1%82%D0%BE%D1%82%D0%BE%20%D0%BF%D0%BE%D0%BA%D0%BE%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5%20%D0%BD%D0%B0%20%D0%BC%D0%BE%D0%B4%D0%B5%D0%BB%D0%B0.%20%D0%9E%D0%BF%D1%82%D0%B8%D0%BC%D0%B0%D0%BB%D0%BD%D0%BE%D1%82%D0%BE%20%D1%80%D0%B0%D0%B7%D0%BF%D1%80%D0%B5%D0%B4%D0%B5%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5%20%D0%BD%D0%B0%20%D1%82%D0%B5%D0%B3%D0%BB%D0%BE%D1%82%D0%BE%2C%20%D0%BA%D0%BB%D0%B0%D1%81%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%BE%D1%82%D0%BE%20%D0%B7%D0%B0%D0%B4%D0%B2%D0%B8%D0%B6%D0%B2%D0%B0%D0%BD%D0%B5%20%D0%BD%D0%B0%20%D0%B7%D0%B0%D0%B4%D0%BD%D0%B8%D1%82%D0%B5%20%D0%BA%D0%BE%D0%BB%D0%B5%D0%BB%D0%B0%20%D0%B8%20%D0%BC%D0%BE%D1%89%D0%BD%D0%B8%D1%82%D0%B5%20%D0%B8%20%D0%B2%D0%B8%D1%81%D0%BE%D0%BA%D0%BE%20%D0%B5%D1%84%D0%B5%D0%BA%D1%82%D0%B8%D0%B2%D0%BD%D0%B8%20%D0%B1%D0%B5%D0%BD%D0%B7%D0%B8%D0%BD%D0%BE%D0%B2%D0%B8%20%D0%B8%20%D0%B4%D0%B8%D0%B7%D0%B5%D0%BB%D0%BE%D0%B2%D0%B8%20%D0%B4%D0%B2%D0%B8%D0%B3%D0%B0%D1%82%D0%B5%D0%BB%D0%B8%20%D1%81%20%D1%82%D0%B5%D1%85%D0%BD%D0%BE%D0%BB%D0%BE%D0%B3%D0%B8%D1%8F%20BMW%20EfficientDynamics%20%D0%BE%D1%81%D0%B8%D0%B3%D1%83%D1%80%D1%8F%D0%B2%D0%B0%D1%82%20%D0%B2%D0%BF%D0%B5%D1%87%D0%B0%D1%82%D0%BB%D1%8F%D0%B2%D0%B0%D1%89%D0%B0%20%D0%B4%D0%B8%D0%BD%D0%B0%D0%BC%D0%B8%D0%BA%D0%B0%20%D0%BF%D1%80%D0%B8%20%D0%BD%D0%B8%D1%81%D1%8A%D0%BA%20%D1%80%D0%B0%D0%B7%D1%85%D0%BE%D0%B4%20%D0%BD%D0%B0%20%D0%B3%D0%BE%D1%80%D0%B8%D0%B2%D0%BE.%20%D0%9D%D0%B0%D0%B9-%D0%B4%D0%BE%D0%B1%D1%80%D0%BE%D1%82%D0%BE%20%D1%81%D1%86%D0%B5%D0%BF%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5%20%D0%BF%D1%80%D0%B8%20%D0%B2%D1%81%D1%8F%D0%BA%D0%B0%D0%BA%D0%B2%D0%B8%20%D1%83%D1%81%D0%BB%D0%BE%D0%B2%D0%B8%D1%8F%20%D1%81%D0%B5%20%D0%B3%D0%B0%D1%80%D0%B0%D0%BD%D1%82%D0%B8%D1%80%D0%B0%20%D0%BE%D1%82%20%D0%B8%D0%BD%D1%82%D0%B5%D0%BB%D0%B8%D0%B3%D0%B5%D0%BD%D1%82%D0%BD%D0%B0%D1%82%D0%B0%20%D1%81%D0%B8%D1%81%D1%82%D0%B5%D0%BC%D0%B0%20%D0%B7%D0%B0%20%D0%B4%D0%B2%D0%BE%D0%B9%D0%BD%D0%BE%20%D0%B7%D0%B0%D0%B4%D0%B2%D0%B8%D0%B6%D0%B2%D0%B0%D0%BD%D0%B5%20BMW%20xDrive.%20%D0%A1%20%D0%BE%D0%B1%D0%BE%D1%80%D1%83%D0%B4%D0%B2%D0%B0%D0%BD%D0%B5%D1%82%D0%BE%20%D0%9B%D0%B8%D0%BD%D0%B8%D1%8F%20Sport%2C%20%D0%9B%D0%B8%D0%BD%D0%B8%D1%8F%20Luxury%20%D0%B8%20%D0%9C%20%D0%A1%D0%BF%D0%BE%D1%80%D1%82%D0%BD%D0%B8%D1%8F%20%D0%BF%D0%B0%D0%BA%D0%B5%D1%82%20BMW%20%D0%A1%D0%B5%D1%80%D0%B8%D1%8F%203%20%D0%A1%D0%B5%D0%B4%D0%B0%D0%BD%20%D0%BC%D0%BE%D0%B6%D0%B5%20%D0%B4%D0%B0%20%D0%B1%D1%8A%D0%B4%D0%B5%20%D0%B0%D0%B4%D0%B0%D0%BF%D1%82%D0%B8%D1%80%D0%B0%D0%BD%D0%BE%20%D0%BA%D1%8A%D0%BC%20%D0%BB%D0%B8%D1%87%D0%BD%D0%B8%D1%82%D0%B5%20%D0%BC%D0%B8%20%D0%B6%D0%B5%D0%BB%D0%B0%D0%BD%D0%B8%D1%8F%20%D0%B8%20%D0%BF%D1%80%D0%B5%D0%B4%D0%BF%D0%BE%D1%87%D0%B8%D1%82%D0%B0%D0%BD%D0%B8%D1%8F.";
         const postOptions = {
             method: "POST",
             uri: `http://semantic.netpeak.bg/`,
@@ -137,9 +138,28 @@ return request(options)
         return Promise.all(promises);
     })
     .then(res => {
-        console.log(dictWordNyms);
+        const baseUrl = "http://www.textontologyproject.org/ontologies/textontologyproject#";
         const filteredDict = _.pickBy(dictWordNyms, _.identity);
+        _.each(filteredDict, (hyponymObj, word) => {
+            hyponymObj.url = `${baseUrl}${word}`;
+            hyponymObj.parents = hyponymObj.parents || [];
+            const candidateChildren = _.intersection(Object.keys(filteredDict).filter(w => w !== word), hyponymObj.hypernyms);
+            hyponymObj.parents = hyponymObj.parents.concat(candidateChildren);
+        });
 
+        let ontologyXml = "";
+
+        _.each(filteredDict, (objInfo, word) => {
+            ontologyXml += `
+            <owl:Class rdf:about="${objInfo.url}">
+                ${ objInfo.parents.map(parent => `<rdfs:subClassOf rdf:resource="${baseUrl}${parent}"/>`).join("\n")}
+                <rdfs:label xml:lang="en">${word}</rdfs:label>
+                <skos:prefLabel xml:lang="en">${word}</skos:prefLabel>
+            </owl:Class>`;
+        });
+
+        const templateText = readFileSync(TemplateFileName).toString();
+        writeFileSync("1.owl", templateText.replace("__ONTOLOGY__", ontologyXml));
     })
     .catch(e => {
         console.error(e);
